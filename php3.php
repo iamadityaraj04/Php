543a6903179864f3c4c4c8405d2426452de3680b
iamadityaraj04 <adityarohitraj@gmail.com>
2022-02-02 10:21:35 +0530

    Variables

---
+++ php3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+
+    <?php
+        $txt = "Hello world!";//in php we use $ symbol to declare variable
+        $x = 5;
+        $y = 10.5;
+        echo $x+$y;
+        
+        echo "<br>";//for line break
+        echo $txt;
+        echo "<br>";
+        
+        $name="Aditya";
+        echo "Hi ".$name."! How are you?";
+    ?>
+</body>
+</html>
