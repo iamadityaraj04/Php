3f02a0d9fc2c5130d1f1b35e00bc5891f8f38d43
iamadityaraj04 <adityarohitraj@gmail.com>
2022-02-02 10:11:25 +0530

    Comments

---
+++ php2.php
@@ -0,0 +1,18 @@
+<!-- Comments in Php -->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// single-line comment
+#  single-line comment
+/*
+multiple line comment
+*/
+$x = 5 /* + 15 */ + 5; //inline comments
+echo $x;
+?>
+
+</body>
+</html>
