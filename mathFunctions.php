d31ce70eb2eea7527335db9528ec2df56d5dfca1
iamadityaraj04 <adityarohitraj@gmail.com>
2022-02-03 19:17:15 +0530

    mathFunctions

---
+++ mathFunctions.php
@@ -0,0 +1,28 @@
+<?php
+    //to print the value of pie
+    echo(pi());
+    echo"<br>";
+    
+    //to find max and min
+    echo"Min: ".(min(3,443,324,63,2,53,-2,0.3));
+    echo"<br>";
+    echo"Max: ".(min(3,443,324,63,2,53,-2,0.3));
+    echo"<br>";
+
+    //to find square root 
+    echo"Square Root of 121: ". sqrt((121));
+    echo"<br>";
+    
+    //to rounding of the floating point numbers
+    echo"Round of 6.3: ". round((6.3));
+    echo"<br>";
+    echo"Round of 0.5: ". round((0.5));
+    echo"<br>";
+    
+    //to print some random number
+    echo"Random Number: ".rand();
+    echo"<br>";
+    echo"Random Number: ".rand(-100,100);
+    echo"<br>";
+    
+?>
