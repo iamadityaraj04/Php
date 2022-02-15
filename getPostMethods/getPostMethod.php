90f82fc525faeda323cdb3fe66813e839d571e2b
iamadityaraj04 <adityarohitraj@gmail.com>
2022-02-15 16:59:33 +0530

    getPostMethods

---
+++ getPostMethods/getPostMethod.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <form action="data.php" method="get">
+        Firstname: <input type="text" name="fname"><br><br>
+        Lastname: <input type="text" name="lname"><br><br>
+        Age: <input type="text" name="age"><br><br>
+        <input type="submit">
+    </form> -->
+    <form action="data.php" method="post">
+        Firstname: <input type="text" name="fname"><br><br>
+        Lastname: <input type="text" name="lname"><br><br>
+        Age: <input type="text" name="age"><br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
