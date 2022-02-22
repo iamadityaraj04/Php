648143084d587d824604f1a3f068e2e3b6fb089f
iamadityaraj04 <adityarohitraj@gmail.com>
2022-02-22 17:14:11 +0530

    formValidation

---
+++ formValidation/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="validation.php" method="POST" >
+        Name: <input type="text" name="name"><br><br>
+        Password: <input type="text" name="pwd"><br><br>
+        Age: <input type="text" name="age"><br><br>
+        <input type="submit" name="save">
+    </form>
+
+</body>
+</html>
---
+++ formValidation/validation.php
@@ -0,0 +1,30 @@
+<!-- 
+    []-> pass the pattern
+    \s-> space
+    \d-> digit
+    \w-> special characters
+    {}-> define range for operation
+    + -> apply rule to all
+-->
+    <?php
+    $namePattern ="/^[a-zA-Z\s]+$/";
+    $agePattern ="/^[0-9]+$/";
+    $pwdPattern ="/^[0-9a-zA-z]+$/";
+    if(isset($_REQUEST['save'])){
+        if(preg_match($namePattern,$_REQUEST['name'])){
+            echo "Name is as per pattern"."</br>";
+        }else{
+            echo "Name is not as per pattern"."</br>";
+        }
+        if(preg_match($pwdPattern,$_REQUEST['pwd'])){
+            echo "Password is as per pattern"."</br>";
+        }else{
+            echo "Password is not as per pattern"."</br>";
+        }
+        if(preg_match($agePattern,$_REQUEST['age'])){
+            echo "Age is as per pattern"."</br>";
+        }else{
+            echo "Age is not as per pattern"."</br>";
+        }
+    }
+    ?>
